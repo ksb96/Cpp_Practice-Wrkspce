de9be85b1b07a448b51521ad95c058238d48482e
ksb96 <52975077+ksb96@users.noreply.github.com>
2022-01-19 22:47:59 +0530

    return statement
    ---return file added

---
+++ return statement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+    //demo of return statement
+    double cube(double num){
+	return num * num * num ;
+    }
+    
+    int main(){
+    {
+    cout<<cube(5.0);
+    return 0; 
+    }
+    }
