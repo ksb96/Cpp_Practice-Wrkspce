90229841d191cbae22180305efd4a05abb92af7a
ksb96 <52975077+ksb96@users.noreply.github.com>
2022-01-19 22:55:01 +0530

    if-else
    ---if-else cond.1 file added

---
+++ if-else.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+    //IF-Else
+    int main(){
+    bool isMale=false;
+    if(isMale){
+    cout << "you are male";
+    }else{
+    cout<<"you are not male";
+    }
+    return 0;
+    }
