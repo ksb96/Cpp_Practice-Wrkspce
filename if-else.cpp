66abd2f643b952f8a2cae19726be6358f4ae4ef7
ksb96 <52975077+ksb96@users.noreply.github.com>
2022-04-29 22:40:26 +0530

    more ex. added

--- if-else.cpp
+++ if-else.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-    //IF-Else
+    //IF-Else 1
     int main(){
     bool isMale=false;
     if(isMale){
@@ -11,3 +11,36 @@ using namespace std;
     }
     return 0;
     }
+////////////////////////////////////////////
+//2
+{
+bool isMale=false;
+bool isTall=true;
+if(isMale && isTall){ //both conditions need to be 
+
+true
+cout << "you are male";
+}else{
+cout<<"you are not male";
+}
+    return 0;
+}
+////////////////////////////////////////////
+//3 OR
+if(isMale || isTall) //any 1 needs to be true
+////////////////////////////////////////////
+//4
+{
+bool isMale=true;
+bool isTall=true;
+if(isMale && isTall){ //both conditions need to be true
+cout << "you are tall male";
+} else if(isMale && !isTall){
+cout<<"you are not tall male";
+} else if(!isMale && isTall){
+cout<<"you are tall but not male";
+}else{
+cout<<"you are not male and nt tall";
+}
+return 0;
+}    
