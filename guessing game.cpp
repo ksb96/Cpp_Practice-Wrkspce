d3121f69ddb5bfaba11ea0e31ea42e3872cf8437
ksb96 <52975077+ksb96@users.noreply.github.com>
2022-04-30 15:10:12 +0530

    init commit

---
+++ guessing game.cpp
@@ -0,0 +1,24 @@
+//starting from main()
+main(){
+int secretNum = 7;
+int guess;
+int guessCount = 0;
+int guessLimit = 3;
+bool outOfGuesses = false;
+
+  //logic
+while(secretNum  1= guess && !outOfGuesses ){
+if(guessCount  < guessLimit ){
+cout << "Enter guess: ";
+cin >> guess;
+guessCount ++;
+}else{
+outOfGuesses  = true;
+}}
+if(outOfGuesses){
+cout << "You Lose!";
+}else{
+cout << "You Win!";
+}
+return 0;
+}
