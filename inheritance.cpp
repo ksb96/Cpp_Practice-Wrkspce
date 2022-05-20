91e67619882439c0ed046e1fcc2de9e3674d9de9
ksb96 <52975077+ksb96@users.noreply.github.com>
2022-05-20 11:39:29 +0530

    init file committed

---
+++ inheritance.cpp
@@ -0,0 +1,43 @@
+class Chef{
+     public:
+          void makeChicken(){
+               cout << "The chef makes chicken" << 
+
+endl;
+          }
+
+          void makeSalad(){
+               cout << "The chef makes salad" << endl;
+          }
+
+          void makeSpecialDish(){
+               cout << "The chef makes a special dish" 
+
+<< endl;
+          }
+};
+
+class ItalianChef : public Chef{
+     public:
+          void makePasta(){
+               cout << "The chef makes pasta" << endl;
+          }
+
+          // override
+          void makeSpecialDish(){
+               cout << "The chef makes chicken parm" 
+
+<< endl;
+          }
+};
+
+int main(){
+
+     Chef myChef();
+     myChef.makeChicken();
+
+     ItalianChef myItalianChef();
+     myItalianChef.makeChicken();
+
+     return 0;
+}
